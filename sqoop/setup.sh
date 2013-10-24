1a0c725fac1255c981badeddb2f702b3157d493e
François Le Lay <mfworx@gmail.com>
2013-10-24 14:44:36 -0400

    use setup phase rather than init phase for sqoop

--- sqoop/setup.sh
+++ sqoop/setup.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-/root/spark-ec2/copy-dir /root/sqoop
+pushd /root
+
+export PATH=$PATH:/root/ephemeral-hdfs/bin
+
+hadoop fs -copyToLocal s3://viadeo-bi/sqoop-1.4.4.bin__hadoop-1.0.0.tar.gz sqoop-1.4.4.bin__hadoop-1.0.0.tar.gz
+tar -xzf sqoop-1.4.4.bin__hadoop-1.0.0.tar.gz
+
+hadoop fs -copyToLocal s3://viadeo-bi/mysql-connector-java-5.1.26.tar.gz mysql-connector-java-5.1.26.tar.gz
+tar -xzf mysql-connector-java-5.1.26.tar.gz
+
+cp mysql-connector-java-5.1.26/mysql-connector-java-5.1.26-bin.jar sqoop-1.4.4.bin__hadoop-1.0.0/lib/
+
+hadoop fs -copyToLocal s3://viadeo-bi/sqoop-site.xml sqoop-site.xml 
+cp sqoop-site.xml sqoop-1.4.4.bin__hadoop-1.0.0/conf
+
+cat >> ~/.bashrc <<EOF 
+export PATH=$PATH:/home/hadoop/sqoop-1.4.4.bin__hadoop-1.0.0/bin
+EOF
+
+. ~/.bashrc
+
+popd
+
+/root/spark-ec2/copy-dir /root/sqoop-1.4.4.bin__hadoop-1.0.0
+
+
 
